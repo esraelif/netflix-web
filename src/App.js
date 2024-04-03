61ef96be3309a1a05ac6276fac12237dd2b77b29
esraelif <elifesratunca@gmail.com>
2024-04-03 13:21:27 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-
-    </div>
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
