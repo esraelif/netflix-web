5de8f4511a205788b2779153bf7d26114a723ab5
esraelif <elifesratunca@gmail.com>
2024-04-03 09:04:08 +0200

    Row.js

--- src/Request.js
+++ src/Request.js
@@ -5,10 +5,10 @@ const key = '47553a11924967134bf1d5f5978b646a'
 
 const requests = {
 
-    requestPopular: ` https://api.themoviedb.org/3/movie/popular?api_key={key}&language=en-US&page=1`,
-    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key={key}&language=en-US&page=1`,
-    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key={key}&language=en-US&page=1`,
-    requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key={key}&language=en-US&page=1`,
+    requestPopular: ` https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+    requestNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${key}&language=en-US&page=1`,
 
 
 }
